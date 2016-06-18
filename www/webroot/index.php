1b3de284eb5c120b233ee712e6ccbe7f3298f68c
root <user.loca@gmail.com>
2016-06-18 18:32:28 +1000

    [add] webroot/index.php:skeleton

---
+++ www/webroot/index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+    <!-- title -->
+    <title>pholder</title>
+
+    <!-- meta -->
+    <meta charset="utf-8">
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author"      content="SitePoint">
+
+    <!-- css -->
+    <link rel="stylesheet" href="css/pholder.css">
+    <link rel="stylesheet" href="css/bar-menu.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/panel-left.css">
+    <link rel="stylesheet" href="css/tooltip-status.css">
+
+    <!--[if lt IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+</head>
+    <body>
+
+        <!-- | pholder -->
+        <div id='pholder'>
+
+            <!-- bar-menu -->
+            <div id='bar-menu'>
+            </div>
+
+            <!-- panel-left -->
+            <div id='panel-left'>
+            </div>
+
+            <!-- main -->
+            <div id='main'>
+            </div>
+
+            <!-- tooltip - status -->
+            <div id='toolitip-status'>
+            </div>
+
+        </div>
+        <!-- pholder | -->
+
+        <!-- javascript -->
+        <script src="js/scripts.js"></script>
+
+    </body>
+</html>
