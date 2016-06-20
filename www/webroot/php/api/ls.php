165d71b2e69397d223a25f362ac8544473eb48da
root <user.loca@gmail.com>
2016-06-21 01:09:21 +1000

    [update] webroot/php/api/ls.php: added file details

--- www/webroot/php/api/ls.php
+++ www/webroot/php/api/ls.php
@@ -10,22 +10,59 @@
 
 
 
+    /** get - file - details **/
+    private function get_file_details($file) {
+                $details = array();
+                $details ['name'] = $file;
+                if($this->input_path != "/"){
+                    $details['path'] = $this->input_path.'/'.$file;
+                } else {
+                    $details['path'] = $this->input_path.$file;
+                }
+                $details['mime'] = mime_content_type($details['path']);
+                $details['icon'] = $this->get_file_icon($details['mime']);
+        return  $details;
+    }
+
+    /** get - file - icon **/
+    private function get_file_icon($mime) {
+        switch($mime) {
+            case 'directory':
+                return 'directory';
+            default:
+                return '';
+        }
+    }
+
+    /** get - files - details **/
+    private function get_files_details($files) {
+                $files_with_details = array();
+                foreach($files as $file) {
+                    $files_with_details[] = $this->get_file_details($file);
+                }
+        return  $files_with_details;
+    }
+
+
+
+
+
     /** input **/
     private function input() {
-        foreach($_POST as $key => $value) {
-            $method = "input_$key";
-            if(method_exists($this, $method)) {
-                $this->$method($value);
-            }
-        }
+                foreach($_POST as $key => $value) {
+                    $method = "input_$key";
+                    if(method_exists($this, $method)) {
+                        $this->$method($value);
+                    }
+                }
         return  true;
     }
 
     /** input - path **/
     private function input_path($path) {
-        if(is_dir($path)){
-            $this->input_path = $path;
-        }
+                if(is_dir($path)){
+                    $this->input_path = $path;
+                }
         return  true;
     }
 
@@ -35,7 +72,8 @@
     /** ls **/
     private function ls() {
         if( ! is_null($this->input_path) ){
-            return $this->set_response_data(scandir($this->input_path));
+            $files = $this->get_files_details(scandir($this->input_path));
+            return   $this->set_response_data($files);
         } else {
             $this->set_response_code(400);
             return false;
