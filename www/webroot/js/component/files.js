3cbfae00001fba4bcb622a46c082b8715949cc3d
root <user.loca@gmail.com>
2016-06-20 23:31:31 +1000

    [update] webroot/js/component/files.js: refresh/clear

--- www/webroot/js/component/files.js
+++ www/webroot/js/component/files.js
@@ -4,9 +4,27 @@ var files = {
 
 
 
+        // clear
+        clear : function() {
+            $("#files").empty();
+        },
+
+
         // refresh
-        refresh : function(path) {
-            console.log(path);
+        refresh : function() {
+            this.clear();
+            $.ajax({
+                        type:       "POST",
+                        data:       $('#path form').serialize(),
+                        url:        "php/api/ls.php",
+                        success:    function(data) {
+                            path.display_200();
+                            console.log(data);
+                        },
+                        error:      function() {
+                            path.display_400();
+                        }
+            });
         }
 
 
