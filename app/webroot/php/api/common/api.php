71034502a65b431e1ca1b8fd214d6db25327ef27
root <user.loca@gmail.com>
2016-07-10 16:34:42 +1000

    [fix] init config location in function instead of variable... ugly

--- app/webroot/php/api/common/api.php
+++ app/webroot/php/api/common/api.php
@@ -9,7 +9,7 @@
     private $response_data = array();
 
     /** config **/
-    private $config = __DIR__."/../../../../config";
+    private $config = null;
 
     /** input **/
     public $input_path = null;
@@ -35,8 +35,16 @@
 
 
 
+    /** get - setting **/
+    public function get_setting() {
+        $this->config = __DIR__."/../../../../config";
+    }
+
     /** get - setting - global - root **/
     public function get_setting_global_root() {
+        if(is_null($this->config)) {
+            $this->get_setting();
+        }
         if(file_exists($this->config.'/global.root') ){
             if($root = trim(file_get_contents($this->config.'/global.root'))) {
                 if(is_dir($root)) {
