102b9d844abd086afd02d982ab0cb739f8590664
root <user.loca@gmail.com>
2016-08-26 13:21:12 +1000

    [update] app/: download.php segemented

--- app/webroot/php/api/download.php
+++ app/webroot/php/api/download.php
@@ -18,38 +18,57 @@
 
 
 
-    /** get **/
-    private function download() {
+    /** | download **/
 
-        // ? - null
-        if(is_null($this->input_path) ){
-            $this->set_response_code(400);
-            return false;
+        private function download() {
+
+            // ? - apache
+
+            // ? - php
+            if( strtolower(substr($_SERVER['SERVER_SOFTWARE'], 0, 3)) == 'php' ){
+                return $this->download_php();
+            }
+
+            // ? - no - match
+            return $this->download_php();
+
         }
 
-        // headers - status - code
-        http_response_code(200);
+        private function download_apache() {
 
-        // headers - download
-        header('Content-Description: File Transfer');
-        header('Content-Disposition: attachment; filename="'.basename($this->input_path).'"');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate');
-        header('Pragma: public');
-        header('Content-Length: ' . filesize($this->input_path));
+        }
 
-        // clean & flush
-        ob_clean();
-        ob_end_flush();
+        private function download_php() {
 
-        // iterate - file - chunks
-        $fp = fopen($this->input_path, 'rb');
-        fpassthru($fp);
+            // ? - null
+            if(is_null($this->input_path) ){
+                $this->set_response_code(400);
+                return false;
+            }
 
-        // return
-        return true;
+            // headers - status - code
+            http_response_code(200);
 
-    }
+            // headers - download
+            header('Content-Description: File Transfer');
+            header('Content-Disposition: attachment; filename="'.basename($this->input_path).'"');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($this->input_path));
+
+            // clean & flush
+            ob_clean();
+            ob_end_flush();
+
+            // iterate - file - chunks
+            $fp = fopen($this->input_path, 'rb');
+            fpassthru($fp);
+
+            // return
+            return true;
+
+        }
 
 
 
